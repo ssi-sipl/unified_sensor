fa3544895fa4bc72b74ef81d1af8211ec9f15a96
Adideb Das <75713088+adideb-das@users.noreply.github.com>
2025-01-30 12:51:43 +0530

    Update bme280.ino

--- bme280.ino
+++ bme280.ino
@@ -1,50 +1,37 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-#define NUM_SAMPLES 10  // Number of readings to average
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
+
+	if (!bme.begin(0x76)) {
+		Serial.println("Could not find a valid BME280 sensor, check wiring!");
+		while (1);
+	}
 }
 
 void loop() {
-  float sensorValue = 0;
+	Serial.print("Temperature = ");
+	Serial.print(bme.readTemperature());
+	Serial.println("*C");
 
-  // Take multiple sensor readings and average them
-  for (int i = 0; i < NUM_SAMPLES; i++) {
-    sensorValue += analogRead(A0);
-    delay(10);  // Short delay to stabilize readings
-  }
-  sensorValue /= NUM_SAMPLES;  // Compute the average
+	Serial.print("Pressure = ");
+	Serial.print(bme.readPressure() / 100.0F);
+	Serial.println("hPa");
 
-  // Serial.print("Averaged Analog Value = ");
-  // Serial.println(sensorValue);
+	Serial.print("Approx. Altitude = ");
+	Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+	Serial.println("m");
 
-  // Convert raw ADC value to voltage
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // Serial.print("Voltage = ");
-  // Serial.print(voltage, 2);
-  // Serial.println(" V");
+	Serial.print("Humidity = ");
+	Serial.print(bme.readHumidity());
+	Serial.println("%");
 
-  // Convert voltage to wind speed (m/s)
-  float wind_speed = mapfloat(voltage, 0.4, 2.0, 0, 32.4);
-  wind_speed = max(0.0, wind_speed);  // Clamp negative values to zero
-
-  // Convert m/s to km/h
-  float speed_kmh = wind_speed * 3.6;
-
-  Serial.print("Wind Speed = ");
-  Serial.print(wind_speed, 2);
-  Serial.println(" m/s");
-
-  Serial.print("Wind Speed = ");
-  Serial.print(speed_kmh, 2);
-  Serial.println(" km/h");
-
-  Serial.println(" ");
-  delay(300);
-}
-
-// Function to map float values
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
-  return max(0.0, (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+	Serial.println();
+	delay(1000);
 }
